960cc523a8cc80a1b50de29b870a0022f562c5e5
Rafat Alam <126316989+rafat-alam@users.noreply.github.com>
2024-08-31 15:15:39 +0530

    aa

---
+++ app.js
@@ -0,0 +1,13 @@
+let eyeButtonPass = document.getElementsByClassName("eye-button-pass");
+let passInputBox = document.getElementsByClassName("pass-input-box")
+let a = 0;
+function changeType() {
+  if(a%2==0) {
+    passInputBox[0].setAttribute("type", "text");
+    a++;
+  } else {
+    passInputBox[0].setAttribute("type", "password");
+    a++;
+  }
+}
+eyeButtonPass[0].addEventListener("click", changeType);
